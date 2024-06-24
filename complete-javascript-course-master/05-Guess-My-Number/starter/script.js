66085fd72bcedd2927b4049d34c931fce5894862
ROG4113 <as0009916@gmail.com>
2024-06-24 22:15:37 +0530

    adding in guess my number

--- complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -1,1 +1,25 @@
 'use strict';
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent='Correct number!';
+
+// document.querySelector('.number').textContent=15;
+// document.querySelector('.score').textContent=10;
+
+// console.log(document.querySelector('.guess').value);
+// document.querySelector('.guess').value=24;
+const number=Math.trunc(Math.random()*20)+1;
+document.querySelector('.number').textContent=number;
+
+document.querySelector('.check').addEventListener('click', function(){
+    const guess=Number(document.querySelector('.guess').value);
+    console.log(guess);
+    if(!guess){
+        document.querySelector('.message').textContent="⛔️ No number!";
+    }else if(guess===number){
+        document.querySelector('.message').textContent='🎉 Correct Number!';
+    }else if(guess<=number){
+        document.querySelector('.message').textContent='📈 Too low!';
+    }else{
+        document.querySelector('.message').textContent='📉 Too high!';
+    }
+})
