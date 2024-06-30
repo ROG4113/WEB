7dded22c29494148c548a64447775d8bcc52d6a0
ROG4113 <as0009916@gmail.com>
2024-07-01 02:14:16 +0530

    adding till spread operator

--- complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -12,6 +12,10 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
+  order: function(starterIndex, mainIndex){
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
   openingHours: {
     thu: {
       open: 12,
@@ -26,4 +30,131 @@ const restaurant = {
       close: 24,
     },
   },
+  orderDelivery({starterIndex=1, mainIndex=0, time='20:00', address}){
+    console.log(`Order received! ${this.starterMenu[starterIndex]} 
+      and ${this.mainMenu[mainIndex]} will be delivered 
+      to ${address} at ${time}.`
+    );
+  },
+  orderPasta(ing1, ing2, ing3){
+    console.log(`Here is your delecious pasta with ${ing1}, ${ing2} and ${ing3}.`);
+  }
 };
+
+///////////////////////////////////////// Rest Pattern and Parameters
+
+
+
+///////////////////////////////////////// The Spread Operator (...)
+
+// const arr=[7, 8, 9];
+// const badNewArr=[1, 3, arr[0], arr[1], arr[2]];
+// console.log(badNewArr);
+
+// const newArr=[1, 2, ...arr];
+// console.log(newArr);
+// console.log(...newArr);
+
+// const newMenu=[...restaurant.mainMenu, 'Gnocci'];
+// console.log(newMenu);
+
+// //copy array
+// const mainMenuCopy=[...restaurant.mainMenu];
+
+// //join 2 arrays
+// const menu=[...restaurant.mainMenu, ...restaurant.starterMenu];
+// console.log(menu);
+
+// const str='Jonas';
+// const letters=[...str, '', 's.'];
+// console.log(letters);
+// console.log(...str);
+
+// const ingredients=[
+//   // prompt("Let's make Pasta, Ingredient 1?"),
+//   // prompt("Ingredient 2?"),
+//   // prompt("Ingredient 3?"),
+// ];
+// console.log(ingredients);
+
+// // restaurant.orderPasta(ingredients[0], ingredients[1], ingredients[2]);
+// restaurant.orderPasta(...ingredients);
+
+// //objects
+// const newRestaurant={foundedIn:1998, ...restaurant, founder:'Guiseppe'};
+// console.log(newRestaurant);
+
+// const restaurantCopy={...restaurant};
+// restaurantCopy.name='Ristorante Roma';
+// console.log(restaurantCopy.name);
+// console.log(newRestaurant.name);
+
+///////////////////////////////////////// Destructuring Objects
+
+// restaurant.orderDelivery({
+//   time: '22:30',
+//   address:'Via del Sole, 21',
+//   mainINdex:2,
+//   starterIndex:2,
+// });
+// restaurant.orderDelivery({
+//   address:'Punjabi Bagh',
+// });
+
+// const {name, openingHours, categories}=restaurant;
+// console.log(name, openingHours, categories);
+
+// const {name:restaurantName, openingHours:hours, categories:tags}=restaurant;
+// console.log(restaurantName, openingHours, tags);
+
+// const {menu=[], starterMenu:starters=[]}=restaurant;
+// console.log(menu, restaurant);
+
+// // mutating variables
+// let a=111;
+// let b=999;
+// const obj={a: 23, b:7, c:14};
+// ({a, b}=obj);
+// console.log(a, b);
+
+// //nested obj
+// const{
+//   fri:{open:o, close:c},
+// }=openingHours;
+// console.log(o, c); 
+
+///////////////////////////////////////// Destructuring Arrays
+
+// const arr=[2, 3, 4];
+// const a=arr[0];
+// const b=arr[1];
+// const c=arr[2];
+
+// const [x, y, z]=arr;
+// console.log(x, y, z);
+// console.log(arr);
+
+// let [main,  , secondary]=restaurant.categories;
+// console.log(main, secondary);
+
+// //swapping
+// // let temp=main;
+// // main=secondary;
+// // secondary=temp;
+// // console.log(main, secondary);
+
+// [main, secondary]=[secondary, main];
+// console.log(main, secondary);
+
+// const [starter, mainCourse]=restaurant.order(2, 0);
+// console.log(starter, mainCourse);
+
+// //nested destructuring
+// const nested=[2, 4, [5, 6]];
+// // const [i, , j]=nested;
+// // console.log(i, j);
+// const [i, , [j, k]]=nested; 
+// console.log(i, j, k);
+
+// const [p=1, q=1, r=1]=[8, 9];
+// console.log(p, q, r);
