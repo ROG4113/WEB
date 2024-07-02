c4d91d8748fa2a5c8931efccd8fdce26972315d2
ROG4113 <as0009916@gmail.com>
2024-07-02 22:13:14 +0530

    completed data structures

--- complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -49,66 +49,319 @@ const restaurant = {
   },
 };
 
+///////////////////////////////////////// String Methods Practice
+
+// const getCode = str => str.slice(0, 3).toUpperCase();
+
+// for (const flight of flights.split('+')) {
+//   const [type, from, to, time] = flight.split(';');
+//   const output = `${type.startsWith('_Delayed') ? '🔴' : ''}${type.replaceAll(
+//     '_',
+//     ' '
+//   )} ${getCode(from)} ${getCode(to)} (${time.replace(':', 'h')})`.padStart(36);
+//   console.log(output);
+// }
+
+///////////////////////////////////////// Coding Challenge #4
+
+// document.body.append(document.createElement('textarea'));
+// document.body.append(document.createElement('button'));
+
+// document.querySelector('button').addEventListener('click', function(){
+//   const text=document.querySelector('textarea').value;
+//   const row=text.split('\n');
+//   console.log(row);
+
+//   for(const [i,el] of row.entries()){
+//     const [first, second]=el.toLowerCase().trim().split('_');
+//     const output=`${first}${second.replace(second[0], second[0].toUpperCase())}`;
+//     console.log(`${output.padEnd(20)}${'✅'.repeat(i + 1)}`);
+//   }
+// });
+
+///////////////////////////////////////// Working With Strings - Part 2
+
+// // Split and join 
+// console.log('a+very+nice+string'.split('+'));
+// console.log('Jonas Schmedtmann'.split(' '));
+
+// const [firstName, lastName] = 'Jonas Schmedtmann'.split(' ');
+
+// const newName = ['Mr.', firstName, lastName.toUpperCase()].join(' ');
+// console.log(newName);
+
+// const capitalizeName = function (name) {
+//   const names = name.split(' ');
+//   const namesUpper = [];
+
+//   for (const n of names) {
+//     // namesUpper.push(n[0].toUpperCase() + n.slice(1));
+//     namesUpper.push(n.replace(n[0], n[0].toUpperCase()));
+//   }
+//   console.log(namesUpper.join(' '));
+// };
+
+// capitalizeName('jessica ann smith davis');
+// capitalizeName('jonas schmedtmann');
+
+// // Padding
+// const message = 'Go to gate 23!';
+// console.log(message.padStart(20, '+').padEnd(30, '+'));
+// console.log('Jonas'.padStart(20, '+').padEnd(30, '+'));
+
+// const maskCreditCard = function (number) {
+//   const str = number + '';
+//   const last = str.slice(-4);
+//   return last.padStart(str.length, '*');
+// };
+
+// console.log(maskCreditCard(64637836));
+// console.log(maskCreditCard(43378463864647384));
+// console.log(maskCreditCard('334859493847755774747'));
+
+// // Repeat
+// const message2 = 'Bad waether... All Departues Delayed... ';
+// console.log(message2.repeat(5));
+
+// const planesInLine = function (n) {
+//   console.log(`There are ${n} planes in line ${'🛩'.repeat(n)}`);
+// };
+// planesInLine(5);
+// planesInLine(3);
+// planesInLine(12);
+
+///////////////////////////////////////// Working With Strings - Part 2
+
+// const airline = 'TAP Air Portugal';
+
+// console.log(airline.toLowerCase());
+// console.log(airline.toUpperCase());
+
+// // Fix capitalization in name
+// const passenger = 'jOnAS'; // Jonas
+// const passengerLower = passenger.toLowerCase();
+// const passengerCorrect =
+//   passengerLower[0].toUpperCase() + passengerLower.slice(1);
+// console.log(passengerCorrect);
+
+// // Comparing emails
+// const email = 'hello@jonas.io';
+// const loginEmail = '  Hello@Jonas.Io \n';
+
+// // const lowerEmail = loginEmail.toLowerCase();
+// // const trimmedEmail = lowerEmail.trim();
+// const normalizedEmail = loginEmail.toLowerCase().trim();
+// console.log(normalizedEmail);
+// console.log(email === normalizedEmail);
+
+// // replacing
+// const priceGB = '288,97£';
+// const priceUS = priceGB.replace('£', '$').replace(',', '.');
+// console.log(priceUS);
+
+// const announcement =
+//   'All passengers come to boarding door 23. Boarding door 23!';
+
+// console.log(announcement.replace('door', 'gate'));
+// console.log(announcement.replaceAll('door', 'gate'));
+// console.log(announcement.replace(/door/g, 'gate'));
+
+// // Booleans
+// const plane = 'Airbus A320neo';
+// console.log(plane.includes('A320'));
+// console.log(plane.includes('Boeing'));
+// console.log(plane.startsWith('Airb'));
+
+// if (plane.startsWith('Airbus') && plane.endsWith('neo')) {
+//   console.log('Part of the NEW Airbus family');
+// }
+
+// // Practice exercise
+// const checkBaggage = function (items) {
+//   const baggage = items.toLowerCase();
+
+//   if (baggage.includes('knife') || baggage.includes('gun')) {
+//     console.log('You are NOT allowed on board');
+//   } else {
+//     console.log('Welcome aboard!');
+//   }
+// };
+
+// checkBaggage('I have a laptop, some Food and a pocket Knife');
+// checkBaggage('Socks and camera');
+// checkBaggage('Got some snacks and a gun for protection');
+
+///////////////////////////////////////// Working With Strings - Part 1
+
+// const airline='TAP Air Portugal';
+// const plane='A320';
+
+// console.log(plane[0]);
+// console.log(plane[1]);
+// console.log(plane[2]);
+// console.log('B737'[0]);
+
+// console.log(airline.length);
+// console.log('B737'.length);
+
+// console.log(airline.indexOf('r'));
+// console.log(airline.lastIndexOf('r'));
+// console.log(airline.indexOf('Portugal'));
+
+// console.log(airline.slice(4));
+// console.log(airline.slice(4, 7));
+
+// console.log(airline.slice(0, airline.indexOf(' ')));
+// console.log(airline.slice(airline.lastIndexOf(' ')+1));
+
+// console.log(airline.slice(-2));
+// console.log(airline.slice(1, -2));
+
+// const checkMiddleSeat=function(seat){
+//   const s=seat.slice(-1);
+//   if(s==='B' || s==='E') console.log('Middle seat');
+//   else console.log('You got lucky');
+// }
+// checkMiddleSeat('11B');
+// checkMiddleSeat('23C');
+// checkMiddleSeat('3E');
+
+// console.log(new String('jonas'));
+// console.log(typeof new String('jonas'));
+// console.log(typeof new String('jonas').slice(1));
+
+///////////////////////////////////////// Coding Challenge #3
+
+// const gameEvents = new Map([
+//   [17, '⚽️ GOAL'],
+//   [36, '🔁 Substitution'],
+//   [47, '⚽️ GOAL'],
+//   [61, '🔁 Substitution'],
+//   [64, '🔶 Yellow card'],
+//   [69, '🔴 Red card'],
+//   [70, '🔁 Substitution'],
+//   [72, '🔁 Substitution'],
+//   [76, '⚽️ GOAL'],
+//   [80, '⚽️ GOAL'],
+//   [92, '🔶 Yellow card'],
+// ]);
+// //1
+// const events=new Set(gameEvents.values());
+// console.log(events);
+
+// //2
+// gameEvents.delete(64);
+// console.log(gameEvents);
+
+// //3
+// console.log(`An event happened, on average, every ${90 / gameEvents.size} minutes`);
+
+// //4
+// for(const [i, el] of gameEvents){
+//   const str1='[FIRST HALF]';
+//   const str2='[SECOND HALF]';
+//   console.log(`${i<45?str1:str2} ${i}: ${el}`);
+// }
+
+///////////////////////////////////////// Maps: Iteration
+
+// const question = new Map([
+//   ['question', 'What is the best programming language in the world?'],
+//   [1, 'C'],
+//   [2, 'Java'],
+//   [3, 'JavaScript'],
+//   ['correct', 3],
+//   [true, 'Correct 🎉'],
+//   [false, 'Try again!'],
+// ]);
+// console.log(question);
+
+// // Convert object to map
+// console.log(Object.entries(openingHours));
+// const hoursMap = new Map(Object.entries(openingHours));
+// console.log(hoursMap);
+
+// // Quiz app
+// console.log(question.get('question'));
+// for (const [key, value] of question) {
+//   if (typeof key === 'number') console.log(`Answer ${key}: ${value}`);
+// }
+// // const answer = Number(prompt('Your answer'));
+// const answer = 3;
+// console.log(answer);
+
+// console.log(question.get(question.get('correct') === answer));
+
+// // Convert map to array
+// console.log([...question]);
+// // console.log(question.entries());
+// console.log([...question.keys()]);
+// console.log([...question.values()]);
+
+///////////////////////////////////////// Maps: Fundamentals
+
+// const rest=new Map();
+// rest.set('name', 'Classico Italiano');
+// rest.set(1, 'Firenze, Italy');
+// rest.set(2, 'Lisbon, Portugal');
+
+// rest
+//   .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organin'])
+//   .set('open', 11)
+//   .set('close', 23)
+//   .set(true, 'We are open :)')
+//   .set(false, 'We are closed :(');
+
+// console.log(rest.get('name'));
+// console.log(rest.get(true));
+// console.log(rest.get(1));
+
+// const time=5;
+// console.log(rest.get(time>rest.get('open') && time<rest.get('close')));
+
+// console.log(rest.has('categories'));
+// rest.delete(2);
+// // rest.clear();
+
+// const arr = [1, 2];
+// rest.set(arr, 'Test');
+// rest.set(document.querySelector('h1'), 'Heading');
+// console.log(rest);
+// console.log(rest.size);
+
+// console.log(rest.get(arr));
+
+///////////////////////////////////////// Sets
+
+// const orderSet=new Set([
+//     'Pasta',
+//     'Pizza',
+//     'Pizza',
+//     'Risotto',
+//     'Pasta',
+//     'Pizza', 
+//   ]);
+//   console.log(orderSet);
+
+//   console.log(new Set('Jonas'));
+  
+//   console.log(orderSet.has('Pizza'));
+//   console.log(orderSet.has('Bread'));
+//   orderSet.add('Garlic Bread');
+//   orderSet.add('Garlic Bread');
+//   orderSet.delete('Risotto');
+//   // orderSet.clear();
+//   console.log(orderSet);
+
+//   for(const order of orderSet) console.log(orderSet);
+
+//   const staff = ['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter'];
+//   const staffUnique=[...new Set(staff)];
+//   console.log(staffUnique);console.log(new Set(['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter']).size);
+
+//   console.log(new Set('aniketsingh').size);
+
 ///////////////////////////////////////// Coding Challenge #2
-
-
-
-///////////////////////////////////////// Looping Objects: Object Keys, Values, and Entries
-
-//property names
-const properties=Object.keys(openingHours);
-console.log(properties);
-
-let openStr=`We are open on ${properties.length} days: `;
-for(const day of properties){
-  openStr+=`${day}, `;
-}
-console.log(openStr);
-
-//Property values
-const values=Object.values(openingHours);
-console.log(values);
-
-//entire object
-const entries=Object.entries(openingHours);
-// console.log(entries);
-
-for(const [day, {open ,close}] of entries){
-  console.log(`on ${day} we open at ${open} ans close at ${close}`);
-}
-
-///////////////////////////////////////// Optional Chaining
-// if(restaurant.openingHours && restaurant.openingHours.mon){
-//   console.log(restaurant.openingHours.mon);
-// }
-
-// //with optional chaining
-// console.log(restaurant.openingHours.mon?.open);
-// console.log(restaurant.openingHours?.mon?.open);
-
-// const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
-// for(const day of days){
-//   const open=restaurant.openingHours[day]?.open ?? 'closed';
-//   console.log(`On ${day}, we open at ${open}`);
-// }
-
-// //methods
-// console.log(restaurant.order?.(0, 1) ?? 'Method does not exist');
-// console.log(restaurant.orderRisotto?.(0, 1) ?? 'Method does not exist');
-
-// const users=[{name:'Jonas', email:'hello@jonas.io'}]
-// console.log(users[0]?.name ?? 'user array empty');
-// console.log(users[1]?.name ?? 'user array empty');
-
-///////////////////////////////////////// The for-of Loop
-// const menu=[...restaurant.starterMenu, ...restaurant.mainMenu];
-// for(const item of menu) console.log(item);
-
-// for(const [i, el] of menu.entries()){
-//   console.log(`${i+1}:${el}`);
-// }  
-
-///////////////////////////////////////// Coding Challenge #1
 
 // const game = {
 //   team1: 'Bayern Munich',
@@ -150,6 +403,78 @@ for(const [day, {open ,close}] of entries){
 //     team2: 6.5,
 //   },
 // };
+// //1
+// for(const [i, el]  of game.scored.entries()){
+//   console.log(`Goal ${i+1}: ${el}`)
+// }
+// //2
+// let avg=0;
+// for(const val of Object.values(game.odds)) avg+=val;
+// console.log(avg/Object.values(game.odds).length);
+
+// //3
+// const {team1, x:draw, team2}=game.odds;
+// console.log(`odds of victory ${game.team1}: ${team1}`);
+// console.log(`odds of draw: ${draw}`);
+// console.log(`odds of victory ${game.team2}: ${team2}`);
+
+///////////////////////////////////////// Looping Objects: Object Keys, Values, and Entries
+
+// //property names
+// const properties=Object.keys(openingHours);
+// console.log(properties);
+
+// let openStr=`We are open on ${properties.length} days: `;
+// for(const day of properties){
+//   openStr+=`${day}, `;
+// }
+// console.log(openStr);
+
+// //Property values
+// const values=Object.values(openingHours);
+// console.log(values);
+
+// //entire object
+// const entries=Object.entries(openingHours);
+// // console.log(entries);
+
+// for(const [day, {open ,close}] of entries){
+//   console.log(`on ${day} we open at ${open} ans close at ${close}`);
+// }
+
+///////////////////////////////////////// Optional Chaining
+
+// if(restaurant.openingHours && restaurant.openingHours.mon){
+//   console.log(restaurant.openingHours.mon);
+// }
+
+// //with optional chaining
+// console.log(restaurant.openingHours.mon?.open);
+// console.log(restaurant.openingHours?.mon?.open);
+
+// const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+// for(const day of days){
+//   const open=restaurant.openingHours[day]?.open ?? 'closed';
+//   console.log(`On ${day}, we open at ${open}`);
+// }
+
+// //methods
+// console.log(restaurant.order?.(0, 1) ?? 'Method does not exist');
+// console.log(restaurant.orderRisotto?.(0, 1) ?? 'Method does not exist');
+
+// const users=[{name:'Jonas', email:'hello@jonas.io'}]
+// console.log(users[0]?.name ?? 'user array empty');
+// console.log(users[1]?.name ?? 'user array empty');
+
+///////////////////////////////////////// The for-of Loop
+// const menu=[...restaurant.starterMenu, ...restaurant.mainMenu];
+// for(const item of menu) console.log(item);
+
+// for(const [i, el] of menu.entries()){
+//   console.log(`${i+1}:${el}`);
+// }  
+
+///////////////////////////////////////// Coding Challenge #1
 
 // //1
 // // const players1=[...game.players[0]];
