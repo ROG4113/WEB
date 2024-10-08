602cdd79fb2bd728bc00d9582ba94f1c5a5ddab1
ROG4113 <as0009916@gmail.com>
2024-10-09 00:40:07 +0530

    adding form

--- ultimate-react-course-main/11-worldwise/starter/src/components/Form.jsx
+++ ultimate-react-course-main/11-worldwise/starter/src/components/Form.jsx
@@ -1,26 +1,48 @@
-// "https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=0&longitude=0"
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import styles from "./Form.module.css";
 import Button from "./Button";
 import { useNavigate } from "react-router-dom";
+import BackButton from "./BackButton";
+import { useUrlPosition } from "../hooks/useUrlPosition";
 
 export function convertToEmoji(countryCode) {
   const codePoints = countryCode
-    .toUpperCase()
-    .split("")
-    .map((char) => 127397 + char.charCodeAt());
+  .toUpperCase()
+  .split("")
+  .map((char) => 127397 + char.charCodeAt());
   return String.fromCodePoint(...codePoints);
 }
 
-function Form() {
-  const navigate=useNavigate();
+const BASE_URL="https://api.bigdatacloud.net/data/reverse-geocode-client"
 
+function Form() {
+  const [lat, lng]=useUrlPosition();
+  const [isLoadingGeocoding, setIsLoadingGeocoding]=useState(false);
   const [cityName, setCityName] = useState("");
   const [country, setCountry] = useState("");
   const [date, setDate] = useState(new Date());
   const [notes, setNotes] = useState("");
+  const [emoji, setEmoji]=useState("");
+
+
+  useEffect(function(){
+    async function fetchCityData(){
+      try{
+        setIsLoadingGeocoding(true);
+        const res=await fetch(`${BASE_URL}?latitude=${lat}&longitude=${lng}`);
+        const data=await res.json();
+        setCityName(data.city || data.locatlity || "");
+        setCountry(data.countryName);
+        setEmoji(convertToEmoji(data.countryCode));
+      }catch(err){   
+      }finally{
+        setIsLoadingGeocoding(false);
+      }
+    }
+    fetchCityData();
+  }, [lat, lng]);
 
   return (
     <form className={styles.form}>
@@ -31,7 +53,7 @@ function Form() {
           onChange={(e) => setCityName(e.target.value)}
           value={cityName}
         />
-        {/* <span className={styles.flag}>{emoji}</span> */}
+        <span className={styles.flag}>{emoji}</span>
       </div>
 
       <div className={styles.row}>
@@ -54,12 +76,7 @@ function Form() {
 
       <div className={styles.buttons}>
         <Button type="primary" >Add</Button>
-        <Button type="back" onClick={(e)=>{
-          e.preventDefault();
-          navigate(-1);
-          }} >
-            &larr; Back
-        </Button>
+        <BackButton />
       </div>
     </form>
   );
---
+++ ultimate-react-course-main/11-worldwise/starter/src/contexts/CitiesContext.jsx
@@ -0,0 +1,55 @@
+import { createContext, useContext, useEffect, useState } from "react";
+
+const URL = `http://localhost:8000/cities`;
+
+const CitiesContext = createContext();
+
+function CitiesProvider({ children }) {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [currentCity, setCurrentCity] = useState({});
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(URL);
+        const data = await res.json();
+        setCities(data);
+      } catch {
+        alert(`There was a error loading data...`);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+
+  async function getCity(id) {
+    try {
+      setIsLoading(true);
+      const res = await fetch(`${URL}/${id}`);
+      const data = await res.json();
+      setCurrentCity(data);
+    } catch {
+      alert(`There was a error loading data...`);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  return (
+    <CitiesContext.Provider value={{ cities, isLoading, currentCity, getCity }}>
+      {children}
+    </CitiesContext.Provider>
+  );
+}
+
+function useCities() {
+  const context = useContext(CitiesContext);
+  if (context === undefined)
+    throw new Error("CitiesContext was used outside the CitiesProvider");
+  return context;
+}
+
+export { CitiesProvider, useCities };
