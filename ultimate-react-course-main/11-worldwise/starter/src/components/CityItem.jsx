cd10bc8a5cccc6b9a6480c17bc1ab5a9d020b6bf
ROG4113 <as0009916@gmail.com>
2024-10-15 22:08:24 +0530

    completed WorldWise(18)

--- ultimate-react-course-main/11-worldwise/starter/src/components/CityItem.jsx
+++ ultimate-react-course-main/11-worldwise/starter/src/components/CityItem.jsx
@@ -3,7 +3,7 @@ import styles from "./CityItem.module.css";
 import { useCities } from "../contexts/CitiesContext";
 
 function CityItem({ city }) {
-  const {currentCity}=useCities();
+  const {currentCity, deleteCity}=useCities();
   const { cityName, emoji, date, id, position } = city;
   const formatDate = (date) =>
     new Intl.DateTimeFormat("en", {
@@ -12,13 +12,18 @@ function CityItem({ city }) {
       year: "numeric",
     }).format(new Date(date));
 
+    function handleDelete(e){
+      e.preventDefault();
+      deleteCity(id);
+    }
+
   return (
     <li>
       <Link className={`${styles.cityItem} ${id===currentCity.id?styles["cityItem--active"]:""}`} to={`${id}?lat=${position.lat}&lng=${position.lng}`}>
         <span className={styles.emoji}>{emoji}</span>
         <h3 className={styles.name}>{cityName}</h3>
         <time className={styles.date}>({formatDate(date)})</time>
-        <button className={styles.deleteBtn}>&times;</button>
+        <button className={styles.deleteBtn} onClick={handleDelete} >&times;</button>
       </Link>
     </li>
   );
--- ultimate-react-course-main/11-worldwise/starter/src/components/Form.jsx
+++ ultimate-react-course-main/11-worldwise/starter/src/components/Form.jsx
@@ -1,11 +1,16 @@
 
 import { useEffect, useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 import styles from "./Form.module.css";
 import Button from "./Button";
 import { useNavigate } from "react-router-dom";
 import BackButton from "./BackButton";
 import { useUrlPosition } from "../hooks/useUrlPosition";
+import Spinner from "./Spinner";
+import Message from "./Message";
+import { useCities } from "../contexts/CitiesContext";
 
 export function convertToEmoji(countryCode) {
   const codePoints = countryCode
@@ -19,6 +24,8 @@ const BASE_URL="https://api.bigdatacloud.net/data/reverse-geocode-client"
 
 function Form() {
   const [lat, lng]=useUrlPosition();
+  const {createCity, isLoading}=useCities();
+  const navigate=useNavigate();
   const [isLoadingGeocoding, setIsLoadingGeocoding]=useState(false);
   const [cityName, setCityName] = useState("");
   const [country, setCountry] = useState("");
@@ -28,6 +35,8 @@ function Form() {
 
 
   useEffect(function(){
+    if(!lat && !lng) return;
+
     async function fetchCityData(){
       try{
         setIsLoadingGeocoding(true);
@@ -44,8 +53,31 @@ function Form() {
     fetchCityData();
   }, [lat, lng]);
 
+  async function handleSubmit(e){
+    e.preventDefault();
+
+    if(!cityName || !date) return;
+
+    const newCity={
+      cityName,
+      country,
+      emoji,
+      date,
+      notes,
+      position:{lat, lng},
+    };
+    await createCity(newCity);
+    navigate("/app/cities");
+  }
+
+  if(isLoadingGeocoding) return <Spinner />;
+
+  if(!lat && !lng) return <Message message="Start by clinking somewhere on the map"/>;
+
   return (
-    <form className={styles.form}>
+    <form className={`${styles.form} ${isLoading?styles.loading:""}`} 
+    onSubmit={handleSubmit}
+    >
       <div className={styles.row}>
         <label htmlFor="cityName">City name</label>
         <input
@@ -58,11 +90,15 @@ function Form() {
 
       <div className={styles.row}>
         <label htmlFor="date">When did you go to {cityName}?</label>
-        <input
+        {/* <input
           id="date"
           onChange={(e) => setDate(e.target.value)}
           value={date}
-        />
+        /> */}
+        <DatePicker onChange={date=>setDate(date)} 
+            selected={date}
+            dateFormat={'dd/MM/yyyy'}
+          />
       </div>
 
       <div className={styles.row}>
