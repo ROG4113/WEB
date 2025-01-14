111ef4f9a29869fa0765038d9e9077113d64ef12
ROG4113 <as0009916@gmail.com>
2025-01-14 18:38:59 +0530

    integrating signup and login

--- complete-node-bootcamp-master/4-natours/starter/controllers/userController.js
+++ complete-node-bootcamp-master/4-natours/starter/controllers/userController.js
@@ -1,9 +1,17 @@
-exports.getAllUsers = (req, res) => {
-    res.status(500).json({
-        status: 'error',
-        message: 'This route is not yet defined!'
+const User = require('./../models/userModel');
+const catchAsync = require('./../utils/catchAsync');
+
+exports.getAllUsers = catchAsync(async (req, res) => {
+    const users = await User.find();
+
+    res.status(200).json({
+        status: 'success',
+        results: users.length,
+        data: {
+            users
+        }
     });
-};
+});
 
 exports.createUser = (req, res) => {
     res.status(500).json({
@@ -15,20 +23,20 @@ exports.createUser = (req, res) => {
 exports.getUser = (req, res) => {
     res.status(500).json({
         status: 'error',
-        message: 'This route is njot yet defined!'
+        message: 'This route is not yet defined!'
     });
 };
 
 exports.updateUser = (req, res) => {
     res.status(500).json({
         status: 'error',
-        message: 'This route is njot yet defined!'
+        message: 'This route is not yet defined!'
     });
 };
 
 exports.deleteUser = (req, res) => {
     res.status(500).json({
         status: 'error',
-        message: 'This route is njot yet defined!'
+        message: 'This route is not yet defined!'
     });
 };
