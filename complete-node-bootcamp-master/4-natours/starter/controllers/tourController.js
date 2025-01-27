b6a8e02a909d8480ea5d38a0088be79f0305d29d
ROG4113 <as0009916@gmail.com>
2025-01-28 03:11:09 +0530

    geospatial queries

--- complete-node-bootcamp-master/4-natours/starter/controllers/tourController.js
+++ complete-node-bootcamp-master/4-natours/starter/controllers/tourController.js
@@ -1,5 +1,5 @@
 /* eslint-disable import/no-useless-path-segments */
-// const AppError = require('../utils/appError');
+const AppError = require('../utils/appError');
 const Tour = require('./../models/tourModel');
 const catchAsync = require('./../utils/catchAsync');
 const factory = require('./handlerFactory');
@@ -25,9 +25,9 @@ exports.createTour = factory.createOne(Tour);
 //     });
 // });
 
-exports.getTour = factory.getOne(Tour, {path:'reviews'});
+exports.getTour = factory.getOne(Tour, { path: 'reviews' });
 
-exports.updateTour=factory.updateOne(Tour);
+exports.updateTour = factory.updateOne(Tour);
 // exports.updateTour = catchAsync(async (req, res, next) => {
 //     const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
 //         new: true
@@ -121,6 +121,33 @@ exports.getMonthlyPlan = catchAsync(async (req, res, next) => {
         status: 'success',
         data: {
             plan
+        }
+    });
+});
+
+exports.getToursWithin = catchAsync(async (req, res, next) => {
+    const { distance, latlng, unit } = req.params;
+    const [lat, lng] = latlng.split(',');
+    const radius=unit==='mi'?distance/3963.2:distance/6378.1;
+
+    if (!lat || !lng) {
+        next(
+            new AppError(
+                'Please provide latitude and longitude in the format lat,lng.',
+                400
+            )
+        );
+    }
+
+    const tours = await Tour.find({
+        startLocation: { $geoWithin: { $centerSphere: [[lng, lat], radius] } }
+    });
+
+    res.status(200).json({
+        status: 'success',
+        results:tours.length,
+        data: {
+            data: tours
         }
     });
 });
--- complete-node-bootcamp-master/4-natours/starter/models/reviewsModel.js
+++ complete-node-bootcamp-master/4-natours/starter/models/reviewsModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Tour = require('./tourModel');
 
 const reviewSchema = new mongoose.Schema({
     review: {
@@ -33,7 +34,9 @@ const reviewSchema = new mongoose.Schema({
     toObject: { virtuals: true },
 });
 
-reviewSchema.pre(/^find/, function(next){
+reviewSchema.index({tour:1, user:1}, {unique:true});
+
+reviewSchema.pre(/^find/, function (next) {
     // this
     // .populate({
     //     path:'tour',
@@ -44,13 +47,53 @@ reviewSchema.pre(/^find/, function(next){
     //     select:'name photo'
     // });
     // next();
-    
+
     this
-    .populate({
-        path:'user',
-        select:'name photo'
-    });
+        .populate({
+            path: 'user',
+            select: 'name photo'
+        });
     next();
+});
+
+reviewSchema.statics.calcAverageRatings = async function (tourId) {
+    const stats = await this.aggregate([
+        {
+            $match: { tour: tourId }
+        },
+        {
+            $group: {
+                _id: '$tour',
+                nRating: { $sum: 1 },
+                avgRating: { $avg: '$rating' }
+            }
+        }
+    ]);
+    if (stats.length > 0) {
+        await Tour.findByIdAndUpdate(tourId, {
+            ratingsQuantity: stats[0].nRating,
+            ratingsAverage: stats[0].avgRating
+        });
+    } else {
+        await Tour.findByIdAndUpdate(tourId, {
+            ratingsQuantity: 0,
+            ratingsAverage: 4.5
+        });
+    }
+};
+
+reviewSchema.post('save', function () {
+    // this points to current review
+    this.constructor.calcAverageRatings(this.tour);
+});
+
+reviewSchema.pre(/^findOneAnd/, async function (next) {
+    this.r = await this.fincdOne();
+    next();
+});
+
+reviewSchema.pre(/^findOneAnd/, async function () {
+    await this.r.constructor.calcAverageRatings(this.r.tour);
 });
 
 const Review = mongoose.model('Review', reviewSchema);
